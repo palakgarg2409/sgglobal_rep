cbf1baa413bc726ef3b9e67affa2f9a88b0ee86c
Vishesh <visheshgargbnl@gmail.com>
2024-02-18 04:24:47 +0530

    final commit with email

--- docs/js/quotation.js
+++ docs/js/quotation.js
@@ -2,26 +2,49 @@ var confirm = document.getElementsByClassName("quotConfirmation");
 
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
+  setTimeout(function(){
+    confirm[0].style.display = "block";
+   }, 2000);
+  console.log("Successfully got into the eventlistener");
 
-  const formData = new FormData(this);
+  var formData = {
+    sender: document.getElementById('email').value,
+    naam : document.getElementById('name').value,
+    msg : document.getElementById('msg').value,
+    contactt : document.getElementById('phone').value,
+    product : document.getElementById('producti').value,
+    quantity : document.getElementById('quantity').value
+  } 
 
   fetch('https://us-central1-sgglobalrep.cloudfunctions.net/sendQuot', {
-      method: 'POST',
-      body: formData
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json' // Set content type to JSON
+    },
+    body: JSON.stringify(formData) // Convert formData to JSON string
   })
   .then(response => {
+    console.log("User Data: " + JSON.stringify(formData));
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('2 : Not okay Response!');
+      }else{
+          console.log('2: Okday Response');
       }
       return response.json();
   })
   .then(data => {
-      console.log(data);
-      alert('Email sent successfully!');
+      if(data.status == 200){
+        console.log('3: Email sent successfully!');
+      }else if(data.status == 500){
+        throw new Error('3: Email sending functions failed! Response: ');
+      }else{
+        console.log("3: Response status not between 200 and 405");
+      }
+      console.log("4: " + data);
   })
   .catch(error => {
-      console.error('There was a problem with your fetch operation:', error);
-      alert('Failed to send email. Please try again later.');
+      console.error('5: There was a problem with your fetch operation:', error);
+      console.log('6: Failed to send email. Please try again later.');
   });
 });
 
--- functions/index.mjs
+++ functions/index.mjs
@@ -1,7 +1,6 @@
 import { onRequest } from "firebase-functions/v2/https";
 import { logger } from "firebase-functions";
 import * as functions from 'firebase-functions';
-import cors from 'cors';
 import nodemailer from 'nodemailer';
 
 // Initialize Nodemailer transporter
@@ -15,127 +14,144 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-const corsHandler = cors({ origin: true });
-
-// Cloud Function to send email
 export const sendEmail = functions.https.onRequest((req, res) => {
-    corsHandler(req, res, () => {
-        try{
-            var sender = req.body.sender;
-            var to = "info@sgglobal.co.in";
-            var name = req.body.naam;
-            var msg = req.body.msg;
-            var contact = req.body.contactt;
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600'); // 1 hour
 
-            // Email message options
-            var mailOptions = {
-                from : 'info@sgglobal.co.in',
-                to : sender,
-                subject : 'Thank You for Your Feedback',
-                html : '<div><h3>Thank You ' + name + ', We are grateful to you for dedicating your time to send us an email. We will get back to you<br></h3></div> ---------------- <br><b><h3>Rajiv Kumar</h3></b> <br>Managing Director  <br>Mobile: +91 98127 00081 <br>Email: info@sgglobal.co.in  <br><br>   <br><br><h3 style="color:grey;">SG GLOBAL</h3>   <br>Address: SCO No. 200, <br>Transport Nagar, Sector - 25, <br>Panipat (PIN - 132103), <br>Haryana, India',
-                // attachments: [
-                //     {
-                //       filename: 'footer_logomail.png', // Change this to the filename of your image
-                //       path: 'docs/assets/footer_logomail.png', // Path to the image file
-                //       cid: 'image1' // Unique identifier for the image
-                //     }
-                // ]
-            }
+    if (req.method === 'OPTIONS') {
+        return res.status(204).send('');
+    }else{
+        console.log(req.body);
+        var boddy = req.body;
+        console.log(boddy.sender);
+        var sender = boddy.sender;
+        var to = "info@sgglobal.co.in";
+        console.log("The email of user: " + sender + ". The datatype of user email: " + typeof(sender));
+        var name = boddy.naam;
+        var msg = boddy.msg;
+        var contact = boddy.contactt;
 
-            var mailOptions2 = {
-                from : 'info@sgglobal.co.in',
-                to : 'info@sgglobal.co.in',
-                subject : 'Feedback for PuffNuts',
-                html : '<div><h4>Feedback Added: </h4></div><div><h3>' + msg + '</h3></div>'+
-                    '<br><div style="color: grey;"> Name: ' + name + '</div>' +
-                    '<div style="color: grey;"> Email: ' + sender + '</div>' + 
-                    '<div style="color: grey;"> Contact: ' + contact + '</div>'
-            }
-
-            // Send email
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.error('Error occurred while sending email:', error);
-                    return res.status(500).json({ success: false, error: 'Failed to send email.' });
-                }
-                console.log('Email sent:', info.response);
-                return res.json({ success: true });
-            });
-
-            transporter.sendMail(mailOptions2, (error, info) => {
-                if (error) {
-                    console.error('Error occurred while sending email:', error);
-                    return res.status(500).json({ success: false, error: 'Failed to send email.' });
-                }
-                console.log('Email sent:', info.response);
-                return res.json({ success: true });
-            });
-        } catch (error) {
-            console.error('An unexpected error occurred:', error);
-            return res.status(500).json({ success: false, error: 'An unexpected error occurred.' });
+        var mailOptions = {
+            from : 'info@sgglobal.co.in',
+            to : sender,
+            subject : 'Thank You for contacting us',
+            html : '<div><h3>Thank You ' + name + ', We are grateful to you for dedicating your time to send us an email. We will get back to you<br></h3></div> ---------------- <b><h3>Rajiv Kumar</h3></b>Managing Director  <br>Mobile: +91 98127 00081 <br>Email: info@sgglobal.co.in  <br><img src="https://drive.google.com/uc?export=view&id=1cOAgWFNHkMgWS9x1tpvOtzV9NJI1sDb3" style="width:150px; height:auto;" /><br>  <h3 style="color:grey;">SG GLOBAL</h3> Address: SCO No. 200, <br>Transport Nagar, Sector - 25, <br>Panipat (PIN - 132103), <br>Haryana, India',
         }
-    });
+
+        var mailOptions2 = {
+            from : 'info@sgglobal.co.in',
+            to : 'info@sgglobal.co.in',
+            subject : 'Feedback',
+            html : '<div><h4>Feedback Added: </h4></div><div><h3>' + msg + '</h3></div>'+
+                '<br><div style="color: grey;"> Name: ' + name + '</div>' +
+                '<div style="color: grey;"> Email: ' + sender + '</div>' + 
+                '<div style="color: grey;"> Contact: ' + contact + '</div>'
+        }
+
+        var errors = 0;
+        var responses = [];
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                errors += 1;
+                responses.push("Sending email to User failed!! Error msg: " + error);
+                console.log(error);
+            }else{
+                responses.push("Email Sent to the User!! Info response: " + info.response);
+                console.log('Email Sent: ' + info.response)
+            }
+        })
+
+        transporter.sendMail(mailOptions2, function(error, info){
+            if(error){
+                errors += 1;
+                responses.append("Sending email to Admin failed!! Error msg: " + error);
+                console.log(error);
+            }else{
+                responses.append("Email Sent to the Admin!! Info response: " + info.response);
+                console.log('Email Sent: ' + info.response)
+            }
+        })
+
+        if(errors == 0){
+            res.status(200).json({ status: 200, EmailSentToUser: responses[0], EmailSentToAdmin: responses[1] });
+        }else{
+            res.status(500).json({ status: 500, EmailSentToUser: responses[0], EmailSentToAdmin: responses[1] });
+        }
+    }
 });
 
 export const sendQuot = functions.https.onRequest((req, res) => {
-    corsHandler(req, res, () => {
-        try{
-            var sender = req.body.sender;
-            var to = "info@sgglobal.co.in";
-            var name = req.body.naam;
-            var msg = req.body.msg;
-            var contact = req.body.contactt;
-            var product = req.body.product;
-            var quantity = req.body.quantity;
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600'); // 1 hour
 
-            // Email message options
-            var mailOptions = {
-                from : 'info@sgglobal.co.in',
-                to : sender,
-                subject : 'Thank You for Your Feedback',
-                html : '<div><h3>Thank You ' + name + ', We are grateful to you for dedicating your time to send us an email. We will get back to you<br></h3></div> ---------------- <br><b><h3>Rajiv Kumar</h3></b> <br>Managing Director  <br>Mobile: +91 98127 00081 <br>Email: info@sgglobal.co.in  <br><br>  <br><br><h3 style="color:grey;">SG GLOBAL</h3>   <br>Address: SCO No. 200, <br>Transport Nagar, Sector - 25, <br>Panipat (PIN - 132103), <br>Haryana, India',
-                // attachments: [
-                //     {
-                //       filename: 'footer_logomail.png', // Change this to the filename of your image
-                //       path: 'docs/assets/footer_logomail.png', // Path to the image file
-                //       cid: 'image1' // Unique identifier for the image
-                //     }
-                // ]
-            }
+    if (req.method === 'OPTIONS') {
+        return res.status(204).send('');
+    }else{
+        console.log(req.body);
+        var sender = req.body.sender;
+        var to = "info@sgglobal.co.in";
+        var name = req.body.naam;
+        var msg = req.body.msg;
+        var contact = req.body.contactt;
+        var product = req.body.product;
+        var quantity = req.body.quantity;
 
-            var mailOptions2 = {
-                from : 'info@sgglobal.co.in',
-                to : 'info@sgglobal.co.in',
-                subject : 'Quotation for ' + product,
-                html : '<div><h4>Quotation Requested for ' + product + '</h4></div>'+
-                    '<br><h3>Quantity: ' + quantity + '</h3>' +
-                    '<br>Message Added: <br>' + msg +
-                    '<br><br>Asked by:' + 
-                    '<br><div style="color: grey;"> Name: ' + name + '</div>' +
-                    '<div style="color: grey;"> Email: ' + sender + '</div>' + 
-                    '<div style="color: grey;"> Contact: ' + contact + '</div>'
-            }
+        var mailOptions = {
+            from : 'info@sgglobal.co.in',
+            to : sender,
+            subject : 'Thank you for your enquiry!',
+            html : '<div><h3>Thank You ' + name + ', We are grateful to you for dedicating your time to send us an email. We will get back to you<br></h3></div> ---------------- <b><h3>Rajiv Kumar</h3></b>Managing Director  <br>Mobile: +91 98127 00081 <br>Email: info@sgglobal.co.in  <br><img src="https://drive.google.com/uc?export=view&id=1cOAgWFNHkMgWS9x1tpvOtzV9NJI1sDb3" style="width:150px; height:auto;" /><br>  <h3 style="color:grey;">SG GLOBAL</h3> Address: SCO No. 200, <br>Transport Nagar, Sector - 25, <br>Panipat (PIN - 132103), <br>Haryana, India',
 
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.error('Error occurred while sending email:', error);
-                    return res.status(500).json({ success: false, error: 'Failed to send email.' });
-                }
-                console.log('Email sent:', info.response);
-                return res.json({ success: true });
-            });
-
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.error('Error occurred while sending email:', error);
-                    return res.status(500).json({ success: false, error: 'Failed to send email.' });
-                }
-                console.log('Email sent:', info.response);
-                return res.json({ success: true });
-            });
-        } catch (error) {
-            console.error('An unexpected error occurred:', error);
-            return res.status(500).json({ success: false, error: 'An unexpected error occurred.' });
+            
         }
-    });
+
+        var mailOptions2 = {
+            from : 'info@sgglobal.co.in',
+            to : 'info@sgglobal.co.in',
+            subject : 'Quotation for ' + product,
+            html : '<div><h4>Quotation Requested for ' + product + '</h4></div>'+
+                '<br><h3>Quantity: ' + quantity + '</h3>' +
+                '<br>Message Added: <br>' + msg +
+                '<br><br>Asked by:' + 
+                '<br><div style="color: grey;"> Name: ' + name + '</div>' +
+                '<div style="color: grey;"> Email: ' + sender + '</div>' + 
+                '<div style="color: grey;"> Contact: ' + contact + '</div>'
+        }
+
+        var errors = 0;
+        var responses = [];
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                errors += 1;
+                responses.push("Sending email to User failed!! Error msg: " + error);
+                console.log(error);
+            }else{
+                responses.push("Email Sent to the User!! Info response: " + info.response);
+                console.log('Email Sent: ' + info.response)
+            }
+        })
+
+        transporter.sendMail(mailOptions2, function(error, info){
+            if(error){
+                errors += 1;
+                responses.append("Sending email to Admin failed!! Error msg: " + error);
+                console.log(error);
+            }else{
+                responses.append("Email Sent to the Admin!! Info response: " + info.response);
+                console.log('Email Sent: ' + info.response)
+            }
+        })
+
+        if(errors == 0){
+            res.status(200).json({ status: 200, EmailSentToUser: responses[0], EmailSentToAdmin: responses[1] });
+        }else{
+            res.status(500).json({ status: 500, EmailSentToUser: responses[0], EmailSentToAdmin: responses[1] });
+        }
+    }
 });
